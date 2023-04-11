2cf3322f3fcd554ae6a82cad4b0291d8bd9db07e
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-04-11 20:28:24 +0530

    updated

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+    get width() {
+        return this.width;
+    }
+    get height(){
+        return this.height;
+    }
 
-class Square extends Animal {}
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(side) {
+        super(side, side);
+    }
+    getPerimeter() {
+        return 4*this.width;
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
